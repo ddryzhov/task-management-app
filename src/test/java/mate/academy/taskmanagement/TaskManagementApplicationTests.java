be44540b55532c5ad6ac9411e82848caf07cc0cc
Daniil Dryzhov <danildryzov@gmail.com>
2024-08-02 19:06:22 +0200

    init

---
+++ src/test/java/mate/academy/taskmanagement/TaskManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package mate.academy.taskmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
